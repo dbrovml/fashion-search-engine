c9a05e26711cee10543481f78b2fd8f6eb6c425b
honeybadger104 <dbrovml@gmail.com>
2025-11-17 09:46:30 +0100

    adding output formatting;

--- src/orchestrator/main.py
+++ src/orchestrator/main.py
@@ -17,8 +17,52 @@ class Orchestrator:
                 filters.clean_query, k=k, filters=filters
             )
         return {
-            "result_items": result_items,
-            "filters": filters,
-            "q_text": q_text,
-            "q_image": q_image,
+            "Items": self._format_items(result_items),
+            "Applied Filters": self._format_filters(filters),
+            "Query": self._format_query(q_text, q_image),
+        }
+
+    def _format_items(self, items):
+        formatted_items = []
+        for item in items:
+            formatted_items.append(
+                {
+                    "CLIP Packshot Image Score": (
+                        f"{item.clip_score1:.2f}" if item.clip_score1 else None
+                    ),
+                    "CLIP Model Image Score": (
+                        f"{item.clip_score2:.2f}" if item.clip_score2 else None
+                    ),
+                    "CLIP Text Score": (
+                        f"{item.clip_score:.2f}" if item.clip_score else None
+                    ),
+                    "ST Text Score": (
+                        f"{item.st_score:.2f}" if item.st_score else None
+                    ),
+                    "Final Score": f"{item.score:.2f}",
+                    "Title": item.title,
+                    "Category": item.category,
+                    "Brand": item.brand,
+                    "Color": item.color,
+                    "Price": f"£{item.price:.2f}",
+                    "Image URL": item.image,
+                    "Product URL": item.url,
+                    "SKU": item.sku,
+                }
+            )
+        return formatted_items
+
+    def _format_query(self, q_text, q_image):
+        return {
+            "Query Image": q_image,
+            "Query Text": q_text,
+        }
+
+    def _format_filters(self, filters):
+        return {
+            "Min Price": filters.min_price,
+            "Max Price": filters.max_price,
+            "Brand": filters.brand,
+            "Category": filters.category,
+            "Color": filters.color,
         }
